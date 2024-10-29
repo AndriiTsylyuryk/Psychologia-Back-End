23874e31731e366421578147f6b07c10befb2c86
AndriiTsylyuryk <navigator.tsylyuryk@gmail.com>
2024-10-29 12:52:44 +0200

    Update calendar.js

--- src/routers/calendar.js
+++ src/routers/calendar.js
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import { OAuth2Client } from 'google-auth-library';
-import { readFile } from 'fs/promises';
-import path from "path";
 import { env } from "../utils/env.js";
 import { google } from "googleapis";
 
@@ -25,7 +22,7 @@ const auth = new google.auth.JWT(
 const calendar = google.calendar({ version: "v3", auth });
 
 router.post("/event", async (req, res) => {
-   const {start, end, title} = req.body;
+   const {start, end, title, } = req.body;
    const event = {
     summary: title,
     start: { dateTime: start, timeZone: "Europe/Kyiv" },
@@ -41,10 +38,10 @@ router.post("/event", async (req, res) => {
         conferenceDataVersion: 1, 
         sendNotifications: true,
     });
-    res.status(200).send("Подія успішно створена!");
+    res.status(200).send("Event created");
   } catch (error) {
-    console.error("Помилка при створенні події:", error);
-    res.status(500).send("Не вдалося створити подію.");
+    console.error( error);
+    res.status(500).send("Bad request");
   }
 });
 
